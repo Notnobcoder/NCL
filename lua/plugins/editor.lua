cc94a2ad92cec1df39ef034aec19b768884a674b
tushar <tushar@lovoj.com>
2024-04-03 21:20:53 +0530

    store list changes

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -143,6 +143,7 @@ return {
         layout_config = { prompt_position = "top" },
         sorting_strategy = "ascending",
         winblend = 0,
+        file_ignore_patterns = { "node_modules/*", "public/*", ".next/*", ".git/*" },
         mappings = {
           n = {},
         },
