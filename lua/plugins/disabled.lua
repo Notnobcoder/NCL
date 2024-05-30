e3d453e7188e99b0deb242e59229e9dd895ff7a8
tushar <tushar@lovoj.com>
2024-05-30 12:27:22 +0530

    disabled unecessary plugisn like mini animate etc

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,4 +1,9 @@
 return {
   -- disable trouble
   { "folke/trouble.nvim", enabled = true },
+  {
+    "echasnovski/mini.animate",
+    enabled = false,
+  },
+  { "nvim-treesitter/nvim-treesitter-context", enabled = false },
 }
